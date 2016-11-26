3e92bb5e6a9801f2285cf45f605e641d509672f7
dawid <talagadawid@gmail.com>
2016-11-26 23:32:25 +0100

    fix in dialog

--- app/dashboard-e/dashboard-e.controller.js
+++ app/dashboard-e/dashboard-e.controller.js
@@ -5,96 +5,74 @@ module('dashboard-e')
 function dashboardEController($http, $scope, $mdDialog) {
   var vm = this;
   vm.query = '';
-
-
-
-  //    $scope.showPrompt = function(ev) {
-  //   // Appending dialog to document.body to cover sidenav in docs app
-  //   var confirm = $mdDialog.prompt()
-  //     .title('What would you name your dog?')
-  //     .textContent('')
-  //     .placeholder('Dog name')
-  //     .ariaLabel('Dog name')
-  //     .initialValue('Buddy')
-  //     .targetEvent(ev)
-  //     .ok('Okay!')
-  //     .cancel('I\'m a cat person');
-  //
-  //   $mdDialog.show(confirm).then(function(result) {
-  //     $scope.status = 'You decided to name your dog ' + result + '.';
-  //   }, function() {
-  //     $scope.status = 'You didn\'t name your dog.';
-  //   });
-  // };
-
-   $scope.showPrompt = function(ev) {
-    $mdDialog.show({
-      controller: () => vm,
- controllerAs: 'vm',
-      templateUrl: '/dashboard-e/dialog.tmpl.html',
-      targetEvent: ev,
-      clickOutsideToClose:true,
-      fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
-    })
-    .then(function(answer) {
-      $scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      $scope.status = 'You cancelled the dialog.';
-    });
-  };
-
-
-
+  vm.currentFunction = '';
+vm.openDalog = openDalog;
 
 
   vm.przyklad = "Przykladowy napis";
   vm.functions;
-  vm.userMatrices =
-  [
-    {
-      "name" : "mDwa",
-      "dimension": "2",
-      "values": [[1,2],[0.4,1]]
-    },
-    {
-      "name" : "mTrzy",
-      "dimension": "3",
-      "values": [[1,2,3],[0.4,1,6],[0.2,0.1,1]]
-    },
-    {
-      "name" : "mDwadwa",
-      "dimension": "2",
-      "values": [[1,2],[0.4,1]]
-    },
-    {
-      "name" : "mTrzytrzy",
-      "dimension": "3",
-      "values": [[1,2,3],[0.4,1,6],[0.2,0.1,1]]
-    }
-  ];
+  vm.userMatrices = [{
+    "name": "mDwa",
+    "dimension": "2",
+    "values": [
+      [1, 2],
+      [0.4, 1]
+    ]
+  }, {
+    "name": "mTrzy",
+    "dimension": "3",
+    "values": [
+      [1, 2, 3],
+      [0.4, 1, 6],
+      [0.2, 0.1, 1]
+    ]
+  }, {
+    "name": "mDwadwa",
+    "dimension": "2",
+    "values": [
+      [1, 2],
+      [0.4, 1]
+    ]
+  }, {
+    "name": "mTrzytrzy",
+    "dimension": "3",
+    "values": [
+      [1, 2, 3],
+      [0.4, 1, 6],
+      [0.2, 0.1, 1]
+    ]
+  }];
 
-  vm.userVectors =
-  [
-    {
-      "name" : "vectorDwa",
-      "values": [0.4,0.6],
-      "dimension": "2"
-    },
-    {
-      "name" : "vectorTrzy",
-      "values": [0,2,5],
-      "dimension": "3"
-    }
-  ];
+  vm.userVectors = [{
+    "name": "vectorDwa",
+    "values": [0.4, 0.6],
+    "dimension": "2"
+  }, {
+    "name": "vectorTrzy",
+    "values": [0, 2, 5],
+    "dimension": "3"
+  }];
 
-  vm.funkcjaNaProbe = proba;
 
+/// FUNTIONS 
   vm.functions = $http.get('dashboard-e/functions.json').then((response) => {
     vm.functions = response.data;
   });
 
-  function proba() {
-    console.log("wywolalem probe, ktora jest przypisana na poczatku, ale zdefiniowana na koncu, tak dla elegancji");
+  function openDalog(ev, funName) {
+    vm.currentFunction = funName;
+    showPrompt(ev);
   }
-  console.log("dashboard");
+
+  function showPrompt(ev) {
+    $mdDialog.show({
+      controller: () => vm,
+      controllerAs: 'vm',
+      templateUrl: '/dashboard-e/dialog.tmpl.html',
+      targetEvent: ev,
+      clickOutsideToClose: true,
+      fullscreen: vm.customFullscreen // Only for -xs, -sm breakpoints.
+    });
+
+  };
 }
